1d551af4aee41c7b329a6d8ad1797043f30efd74
Eric Separovic <ericseparovic07@gmail.com>
2023-01-09 23:25:16 -0300

    Add new line in document txt

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   //   Lista de palabras para buscar
-  const wordsInsert = [
-    "hello",
-    "arise",
-    "broke",
-    "array",
-    "dfsdfadf",
-    "in",
-    "34",
-  ];
-  const wordsDictionary = [];
+  const wordsInsert = ["hello", "arise", "broke", "array", "in"];
 
-  for (let i = 0; i < wordsInsert.length; i++) {
-    await page.goto("https://www.wordreference.com/");
+  let words = "";
+  try {
+    for (let i = 0; i < wordsInsert.length; i++) {
+      await page.goto("https://www.wordreference.com/");
 
-    await page.type(".ac-input", `${wordsInsert[i]}`);
-    await page.click(".submit-button");
+      await page.type(".ac-input", `${wordsInsert[i]}`);
+      await page.click(".submit-button");
 
-    try {
       await page.waitForSelector(".headerWord");
+      await page.waitForSelector(".even .ToWrd");
+      await page.waitForSelector(".even .FrEx");
+
       const wordsAll = await page.evaluate(() => {
-        const wordHTML = document.querySelector(".headerWord").innerText;
+        const word = document.querySelector(".headerWord").innerText;
         const meaning =
           document.querySelector(".even .ToWrd").firstChild.nodeValue;
         const example = document.querySelector(".even .FrEx").innerText;
 
-        return {
-          word: wordHTML,
-          meaning: meaning,
-          example: example,
-        };
+        return `${word};  ${meaning}; ${example} \n`;
       });
 
-      wordsDictionary.push(wordsAll);
-    } catch (err) {
-      console.log(err);
+      words += wordsAll;
+      console.log(words);
     }
+
+    await browser.close();
+  } catch (err) {
+    console.log(err);
   }
 
-  console.log(wordsDictionary);
+  let promiseWriteFile = new Promise((resolve, reject) => {
+    fs.writeFile("example.txt", words, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
 
-  // await browser.close();
+  promiseWriteFile
+    .then(() => {
+      console.log("ok");
+    })
+    .catch(() => {
+      console.log("Error", error);
+    });
 })();
