433204cfec40bafa797661a3cfe761c07fc72696
Eric Separovic <ericseparovic07@gmail.com>
2023-01-12 20:59:10 -0300

    Create txt

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const prompts = require("prompts");
+let words = "";
 
+//Input of words
 (async () => {
+  const response = await prompts({
+    type: "text",
+    name: "words",
+    message: "Insert words",
+  });
+
+  console.log(response.words);
+  //   findWords(response.words);
+})();
+
+//   Lista de palabras para buscar
+
+async function findWords(wordsInput) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  //   Lista de palabras para buscar
-  const wordsInsert = ["hello", "arise", "broke", "array", "in"];
-
-  let words = "";
   try {
-    for (let i = 0; i < wordsInsert.length; i++) {
+    for (let i = 0; i < wordsInput.length; i++) {
       await page.goto("https://www.wordreference.com/");
 
-      await page.type(".ac-input", `${wordsInsert[i]}`);
+      await page.type(".ac-input", `${wordsInput[i]}`);
       await page.click(".submit-button");
 
       await page.waitForSelector(".headerWord");
-      await page.waitForSelector(".even .ToWrd");
-      await page.waitForSelector(".even .FrEx");
+      await page.waitForSelector(".even");
 
-      const wordsAll = await page.evaluate(() => {
-        const word = document.querySelector(".headerWord").innerText;
-        const meaning =
-          document.querySelector(".even .ToWrd").firstChild.nodeValue;
-        const example = document.querySelector(".even .FrEx").innerText;
-
-        return `${word}; ${word}; ${meaning}; ${example} \n`;
-      });
+      const wordsAll = await getData(page);
 
       words += wordsAll;
+      console.log(words);
     }
 
-    await browser.close();
+    // await browser.close();
   } catch (err) {
     console.log(err);
   }
 
+  createTxt(words);
+}
+
+function createTxt(words) {
   let promiseWriteFile = new Promise((resolve, reject) => {
     fs.writeFile("example.txt", words, (error) => {
       if (error) {
@@ -54,4 +63,15 @@ const fs = require("fs");
     .catch(() => {
       console.log("Error", error);
     });
-})();
+}
+
+async function getData(page) {
+  return await page.evaluate(() => {
+    const word = document.querySelector(".headerWord").innerText;
+    const meaning = document.querySelector(".even .ToWrd").firstChild.data;
+    const exampleES = document.querySelector(".even .ToEx span i").innerHTML;
+    const exampleUS = document.querySelector(".even .FrEx span").innerHTML;
+
+    return `${word}; ${meaning}; ${exampleES}; ${exampleUS} \n`;
+  });
+}
